486b6c440f403beb7e4595e189fb43afd90f3db5
Alex Lende <ajlende@gmail.com>
2020-07-24 08:10:37 -0600

    Customize template

--- README.md
+++ README.md
@@ -1,20 +1,36 @@
-# cra-template-typescript
+# cra-template-ajlende
 
-This is the official TypeScript template for [Create React App](https://github.com/facebook/create-react-app).
+This is a template for [Create React App](https://github.com/facebook/create-react-app) with additional configurations that I typically use when creating a new React app.
 
-To use this template, add `--template typescript` when creating a new app.
+In addition to the default CRA features, it includes:
+
+- [TypeScript](https://www.typescriptlang.org/)
+- [Chakra UI](https://chakra-ui.com/) & [Emotion](https://emotion.sh)
+- [Prettier](https://prettier.io/)
+- [ESLint](https://eslint.org/) tweaks
+- New scripts `lint` and `format`
+- Pre-commit lint and format hook
+
+And in order to simplify things, it removes:
+
+- Google web-vitals
+- PWA service worker
+
+To use this template, first clone the project, then add `--template ./cra-template-ajlende` when creating a new app.
 
 For example:
 
 ```sh
-npx create-react-app my-app --template typescript
+git clone https://github.com/ajlende/cra-template-ajlende.git
 
-# or
+# then
 
-yarn create react-app my-app --template typescript
+yarn create react-app my-app --template file:./cra-template-ajlende
 ```
 
 For more information, please refer to:
 
 - [Getting Started](https://create-react-app.dev/docs/getting-started) – How to create a new app.
 - [User Guide](https://create-react-app.dev) – How to develop apps bootstrapped with Create React App.
+
+MIT © [Alex Lende](https://github.com/ajlende)
--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { keyframes } from "@emotion/core"
+import { Box, Code, Flex, Icon, Link, Text } from "@chakra-ui/core"
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+    <Box textAlign="center">
+      <Flex
+        as="header"
+        align="center"
+        justify="center"
+        direction="column"
+        minH="100vh"
+        color="white"
+        backgroundColor="#282c34"
+        fontSize="calc(10px + 2vmin)"
+      >
+        <Icon
+          name="logo"
+          height="40vmin"
+          width="auto"
+          animation={`${spin} infinite 20s linear`}
+          pointerEvents="none"
+          color="#61dafb"
+        />
+        <Text marginY="1em">
+          Edit <Code>src/App.tsx</Code> and save to reload.
+        </Text>
+        <Link
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          isExternal
+          color="#61dafb"
+          textDecor="underline"
         >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </Link>
+      </Flex>
+    </Box>
+  )
 }
 
-export default App;
+export default App
