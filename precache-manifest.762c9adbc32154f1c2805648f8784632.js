1425ee83338c4d76fcfd0af725b0ff367fda9631
Alex Lende <ajlende@gmail.com>
2020-07-24 15:00:39 -0600

    Updates

---
+++ precache-manifest.762c9adbc32154f1c2805648f8784632.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2639b81ee68f5cd4f9bff746c97f6978",
+    "url": "/cra-template-ajlende/index.html"
+  },
+  {
+    "revision": "9965df3b1381912bde31",
+    "url": "/cra-template-ajlende/static/js/2.4ce5d0ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cra-template-ajlende/static/js/2.4ce5d0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91359de7abe6809ec53a",
+    "url": "/cra-template-ajlende/static/js/main.f99179ea.chunk.js"
+  },
+  {
+    "revision": "6ac5d1c2c308b6ce3c7a",
+    "url": "/cra-template-ajlende/static/js/runtime-main.b35d729e.js"
+  }
+]);
